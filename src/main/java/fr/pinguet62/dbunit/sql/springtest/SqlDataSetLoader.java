1b98031b980b78d3d244bc7858756f81c59e303b
PINGUET Julien <jpinguet-prest@redoute.fr>
2016-12-06 17:39:52 +0100

    Fix: Maven version + README

--- src/main/java/fr/pinguet62/dbunit/sql/springtest/SqlDataSetLoader.java
+++ src/main/java/fr/pinguet62/dbunit/sql/springtest/SqlDataSetLoader.java
@@ -6,9 +6,11 @@ import org.dbunit.dataset.IDataSet;
 import org.springframework.core.io.Resource;
 
 import com.github.springtestdbunit.dataset.AbstractDataSetLoader;
+import com.github.springtestdbunit.dataset.DataSetLoader;
 
 import fr.pinguet62.dbunit.sql.ext.SqlDataSet;
 
+/** {@link DataSetLoader} implementation using {@link SqlDataSet}. */
 public class SqlDataSetLoader extends AbstractDataSetLoader {
 
     @Override
